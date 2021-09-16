639476f54b647a919f6f86fc0d09bec0f17eb19b
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-09-17 00:55:13 +0600

    Result page and components

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,11 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Quiz from './pages/Quiz';
+import Result from './pages/Result';
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Result />
     </Layout>
   );
 }
---
+++ src/components/pages/Result.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Summary from '../result/Summary'
+import Analysis from '../result/Analysis'
+const Result = () => {
+    return (
+        <React.Fragment>
+
+            <Summary />
+            <Analysis />
+
+        </React.Fragment>
+    )
+}
+
+export default Result
