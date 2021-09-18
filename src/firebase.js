ad892937df7def90dd7440b447706382b335879f
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-09-19 00:33:31 +0600

    Adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import {initializeApp} from 'firebase/app'
+
+
+const app = initializeApp({
+    
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+
+})
+
+export default app
